1a1bc6f0f0e03003454ca10b7e6e59ac8b972709
nilSinger <niilz@outlook.de>
2020-01-25 12:25:02 +0100

    commit changes in chapter 07 of which I wasn't aware

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# USAGE:
+# Rename RUST-build (VMtranlator compiled for x86_64-linux-musl) to "VM"
+# Put "VMTranslator.py" and "VM" in same directory and zip it
+# (Make sure "VMTranslator.py" has executing rights)
+# Upload zip as projecX.zip (X = number of project e.g. 8)
+
 import subprocess
 import sys
 
