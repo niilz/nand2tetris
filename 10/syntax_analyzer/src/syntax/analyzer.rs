ae723f6b3ced3662d1ed64463fbb4680cc6fd973
nilSinger <niilz@outlook.de>
2020-02-02 13:59:18 +0100

    correct typos in tests and write beginning of subroutine body incl parameterList and varDec

--- 10/syntax_analyzer/src/syntax/analyzer.rs
+++ 10/syntax_analyzer/src/syntax/analyzer.rs
@@ -6,12 +6,13 @@ static VALID_SUBROUTINE_KEYWORDS: &[&str] = &["constructor", "function", "method
 pub fn analyze_tokens(tokens: Vec<Token>) -> String {
     let mut token_stream = tokens.iter().peekable();
 
-    let mut class_tree = String::new();
-
+    
     let class_keyword = token_stream.next().unwrap();
     if class_keyword.value != "class" {
         panic!("class files need to start with class declerationr");
     }
+
+    let mut class_tree = String::from("<class>");
     class_tree.push_str(&class_keyword.to_xml());
 
     let class_name = token_stream.next().unwrap();
@@ -19,21 +20,22 @@ pub fn analyze_tokens(tokens: Vec<Token>) -> String {
         panic!("classes need a valid Class-Identifier");
     }
     class_tree.push_str(&class_name.to_xml());
-    let open_curly_brace = token_stream.next().unwrap();
-    class_tree.push_str(&open_curly_brace.to_xml());
+
+    let class_open_curly = token_stream.next().unwrap();
+    class_tree.push_str(&class_open_curly.to_xml());
 
     // parse the body
-    let body = build_body(&mut token_stream);
+    let body = build_class_body(&mut token_stream);
     class_tree.push_str(&body);
     
     // after body has finished, close class with closing curly brace
     let closing_curly_brace = token_stream.next().unwrap();
     class_tree.push_str(&closing_curly_brace.to_xml());
 
-    class_tree
+    class_tree + "</class>"
 }
 
-fn build_body(mut token_tail: &mut TokenStream) -> String {
+fn build_class_body(mut token_tail: &mut TokenStream) -> String {
     let mut body_xml = String::new();
 
     let next_token = token_tail.peek();
@@ -43,7 +45,7 @@ fn build_body(mut token_tail: &mut TokenStream) -> String {
     }
 
     if token_tail.peek().unwrap().value != "}" {
-        let subroutines = compile_subroutine(&mut token_tail, "<subroutineDec>");
+        let subroutines = compile_subroutine(&mut token_tail, "");
         body_xml.push_str(&subroutines);
     }
 
@@ -63,26 +65,30 @@ fn compile_class_vars(mut token_tail: &mut TokenStream, class_vars_xml: &str) ->
 
 // soubroutine-compiler
 fn compile_subroutine(mut token_tail: &mut TokenStream, subroutine_xml: &str) -> String {
+    let mut result_subroutine_xml = String::from("<subroutineDec>");
     // Add suroutine-identifier, type and subroutine name
-    let mut result_subroutine_xml: String = token_tail
-                                    .take(3)
-                                    .map(|token| token.to_xml())
-                                    .collect();
-    // Add paramlist
-    result_subroutine_xml.push_str(&compile_paramlist(&mut token_tail));
-
-    // Add subroutine-body
-    //TODO result_subroutine_xml.push_str(compile_subroutine_body(...));
+    token_tail
+        .take(3)
+        .map(|t| {println!("TOKEN {:?}", t); t})
+        .for_each(|token| result_subroutine_xml.push_str(&token.to_xml()));
     
-    //  Process more subroutines if available
-    if VALID_SUBROUTINE_KEYWORDS.contains(&&token_tail.peek().unwrap().value.as_str()) {
-        return compile_subroutine(&mut token_tail, "<subroutineDec>");
-    }
+    let param_list = compile_paramlist(&mut token_tail);
+    result_subroutine_xml.push_str(&param_list);
+
+    // Start subroutine-body
+    result_subroutine_xml.push_str("<subroutineBody>");
+    let subroutine_opening_curly = token_tail.next().unwrap();
+    result_subroutine_xml.push_str(&subroutine_opening_curly.to_xml());
+    // Add code in subroutine-body
+    result_subroutine_xml.push_str(&compile_subroutine_body(&mut token_tail));
     // End subroutine
+    let subroutine_closing_curly = token_tail.next().unwrap();
+    result_subroutine_xml.push_str(&subroutine_closing_curly.to_xml());
+    result_subroutine_xml.push_str("</subroutineBody>");
     result_subroutine_xml + "</subroutineDec>"
 }
 
-// Parameterlist compiler
+// Compile PARAM (part of subroutine)
 fn compile_paramlist(mut param_decleration: &mut TokenStream) -> String {
     let mut paramlist_xml = get_paranthese(&mut param_decleration);
     paramlist_xml.push_str("<parameterList>");
@@ -96,17 +102,45 @@ fn compile_paramlist(mut param_decleration: &mut TokenStream) -> String {
         paramlist_xml.push_str(&token.to_xml());
     }
 }
-
 // param-helper
 fn get_paranthese(token_tail: &mut TokenStream) -> String {
     token_tail.next().unwrap().to_xml()
 }
 
+// Compile SOUBROUTINE-BODY (part of subroutine)
+fn compile_subroutine_body(mut token_tail: &mut TokenStream) -> String {
+    let mut result_sub_body_xml = String::new();
+    // add var-decleration if there are any
+    loop {
+        let next_token = token_tail.peek().unwrap();
+        if next_token.value != "var" {
+            break;
+        }
+        result_sub_body_xml.push_str(&compile_var_dec(&mut token_tail));
+    }
+    // TODO: compile_statements (let, if, else, while, do, return)
+    result_sub_body_xml
+}
+// Compile VAR-DECLERATION (part of subroutine-body)
+fn compile_var_dec(token_tail: &mut TokenStream) -> String {
+    let mut var_dec_xml = String::from("<varDec>");
+    loop {
+        let next_token = token_tail.next().unwrap();
+        if next_token.value == ";" {
+            var_dec_xml.push_str(&next_token.to_xml());
+            return var_dec_xml + "</varDec>";
+        }
+        var_dec_xml.push_str(&next_token.to_xml());
+    }
+}
+
+// Compile IF
 fn compile_if() -> String {
     let body: Vec<Token> = Vec::new();
     
     format!("<ifStatement>\n{:?}\n</ifStatement>", body)
 }
+
 
 // Controle-Flow-Helpers
 fn is_class_var_start(maybe_token: Option<&&Token>) -> bool {
@@ -122,6 +156,10 @@ fn is_class_var_start(maybe_token: Option<&&Token>) -> bool {
 static dummy_code: &'static str = "\
     class Great {\
        field int x = 5;\
+       function char myfunc(int age, boolean isCool) {\
+           var char letter;\
+           var int max, min;\
+       }\
     }";
 #[test]
 #[should_panic]
@@ -129,14 +167,15 @@ fn files_without_class_keyword_panic() {
     let mock_token = Token {token_type: TokenType::Keyword, value: String::from("NOCLASS") };
     analyze_tokens(vec![mock_token]);
 }
+// BIG - ALL - TEST
 #[test]
 fn vec_of_tokens_gets_compiled() {
     let mock_tokens = tokenize(dummy_code);
     let mock_result_xml = "\
         <class>\
             <keyword> class </keyword>\
-                <symbol> { </symbol>\
-                <identifier> Great </identifier>\
+            <identifier> Great </identifier>\
+            <symbol> { </symbol>\
                 <classVarDec>\
                     <keyword> field </keyword>\
                     <keyword> int </keyword>\
@@ -145,16 +184,47 @@ fn vec_of_tokens_gets_compiled() {
                     <integerConstant> 5 </integerConstant>\
                     <symbol> ; </symbol>\
                 </classVarDec>\
+                <subroutineDec>\
+                    <keyword> function </keyword>\
+                    <keyword> char </keyword>\
+                    <identifier> myfunc </identifier>\
+                    <symbol> ( </symbol>\
+                    <parameterList>\
+                        <keyword> int </keyword>\
+                        <identifier> age </identifier>\
+                        <symbol> , </symbol>\
+                        <keyword> boolean </keyword>\
+                        <identifier> isCool </identifier>\
+                    </parameterList>\
+                    <symbol> ) </symbol>\
+                        <subroutineBody>\
+                        <symbol> { </symbol>\
+                        <varDec>\
+                            <keyword> var </keyword>\
+                            <keyword> char </keyword>\
+                            <identifier> letter </identifier>\
+                            <symbol> ; </symbol>\
+                        </varDec>\
+                        <varDec>\
+                            <keyword> var </keyword>\
+                            <keyword> int </keyword>\
+                            <identifier> max </identifier>\
+                            <symbol> , </symbol>\
+                            <identifier> min </identifier>\
+                            <symbol> ; </symbol>\
+                        </varDec>\
+                        <symbol> } </symbol>\
+                    </subroutineBody>\
+                </subroutineDec>\
                 <symbol> } </symbol>\
         </class>";
-    analyze_tokens(mock_tokens);
+    assert_eq!(analyze_tokens(mock_tokens), mock_result_xml);
 }
 
-// body-builder-TESTS
+// class-body-builder-TESTS
 #[test]
-fn body_is_correct() {
-    println!("{}", dummy_code);
-    let dummy_tokens = tokenize("field int x;");
+fn class_body_is_correct() {
+    let dummy_tokens = tokenize("field int x; }");
     let result_xml = "\
             <classVarDec>\
                 <keyword> field </keyword>\
@@ -163,7 +233,7 @@ fn body_is_correct() {
                 <symbol> ; </symbol>\
             </classVarDec>\
             ";
-    assert_eq!(build_body(&mut dummy_tokens.iter().peekable()), result_xml);
+    assert_eq!(build_class_body(&mut dummy_tokens.iter().peekable()), result_xml);
 }
 
 // Class-var-TESTS
@@ -239,10 +309,92 @@ fn field_and_static_vars_compile() {
 #[test]
 fn subroutine_compiled() {
     let dummy_subroutine = "\
-        function void myfunc(param1, param2) {\
-            
+        function char myfunc(int age, boolean isCool) {\
+            var char letter;\
+            var int max, min;\
         }";
-    unimplemented!();
+    let dummy_subroutine_tokens = tokenize(dummy_subroutine);
+    let dummy_subroutine_xml = "\
+        <subroutineDec>\
+            <keyword> function </keyword>\
+            <keyword> char </keyword>\
+            <identifier> myfunc </identifier>\
+            <symbol> ( </symbol>\
+            <parameterList>\
+                <keyword> int </keyword>\
+                <identifier> age </identifier>\
+                <symbol> , </symbol>\
+                <keyword> boolean </keyword>\
+                <identifier> isCool </identifier>\
+            </parameterList>\
+            <symbol> ) </symbol>\
+                <subroutineBody>\
+                <symbol> { </symbol>\
+                <varDec>\
+                    <keyword> var </keyword>\
+                    <keyword> char </keyword>\
+                    <identifier> letter </identifier>\
+                    <symbol> ; </symbol>\
+                </varDec>\
+                <varDec>\
+                    <keyword> var </keyword>\
+                    <keyword> int </keyword>\
+                    <identifier> max </identifier>\
+                    <symbol> , </symbol>\
+                    <identifier> min </identifier>\
+                    <symbol> ; </symbol>\
+                </varDec>\
+                <symbol> } </symbol>\
+            </subroutineBody>\
+        </subroutineDec>";
+    assert_eq!(compile_subroutine(&mut dummy_subroutine_tokens.iter().peekable(), ""), dummy_subroutine_xml);
+}
+
+// Subroutine-Body-TESTS
+#[test]
+fn subroutine_body_compiles() {
+    let mock_body = "\
+        var int num, count;\
+        var boolean isOpen;\
+    }";
+    // TODO: add statments
+    let mock_body_tokens = tokenize(mock_body);
+    let mock_body_xml = "\
+        <varDec>\
+            <keyword> var </keyword>\
+            <keyword> int </keyword>\
+            <identifier> num </identifier>\
+            <symbol> , </symbol>\
+            <identifier> count </identifier>\
+            <symbol> ; </symbol>\
+        </varDec>\
+        <varDec>\
+            <keyword> var </keyword>\
+            <keyword> boolean </keyword>\
+            <identifier> isOpen </identifier>\
+            <symbol> ; </symbol>\
+        </varDec>";
+    assert_eq!(compile_subroutine_body(&mut mock_body_tokens.iter().peekable()), mock_body_xml);
+}
+
+fn subroutine_vars_compile() {
+    let mock_body = "\
+        var int num, count;\
+        var boolean isOpen;";
+    let mock_body_xml = "\
+            <varDec>\
+                <keyword> var </keyword>\
+                <keyword> int </keyword>\
+                <identifier> num </identifier>\
+                <symbol> , </symbol>\
+                <identifier> count </identifier>\
+                <keyword> var </keyword>\
+                <keyword> boolean </keyword>\
+                <identifier> isOpen </identifier>\
+                <symbol> ; </symbol>\
+            </varDec>";
+    let mock_body_tokens = tokenize(mock_body);
+    assert_eq!(compile_var_dec(&mut mock_body_tokens.iter().peekable()), mock_body_xml);
 }
 
 // parameterList-TEST
@@ -274,6 +426,7 @@ fn parameterlist_compiles() {
 
 // If Statement-TESTS
 #[test]
+#[ignore]
 fn if_statement_gets_compiled() {
     let if_xml = "TEST";
     assert_eq!(compile_if(), if_xml);
